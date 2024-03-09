31bda85c8cace098c50356867dade277dd3450a5
swagatika977 <swagatikadasmohapatra02@gmail.com>
2024-03-09 10:49:16 +0530

    first commit

---
+++ app/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from app.models import*
+class ProductModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields='__all__'
